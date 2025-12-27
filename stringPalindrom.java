c89260b1734827bb49ed8dc14f4f081f05ebea56
Pranav Firame <pranavfirame06@gmail.com>
2025-12-27 20:15:44 +0530

    Palindrom String

---
+++ stringPalindrom.java
@@ -0,0 +1,20 @@
+public class stringPalindrom {
+    static boolean isPalindrom(String s){
+        int n = s.length() - 1;
+        int i = 0;
+        while(i<=(n/2)){
+            if(s.charAt(i)!=s.charAt(n-i)){
+                System.out.println("thia string is not palindrom");
+                return false;
+            }
+            i++;
+        }
+        System.out.println("palindrom string...");
+        return true;
+    }
+
+    public static void main(String args[]){
+        String name = new String("HEHj");
+        isPalindrom(name);
+    }    
+}

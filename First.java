590fd1a18f6dd56e8dc46f5cac3c5b9aef4f1e0f
Pranav Firame <pranavfirame06@gmail.com>
2025-12-27 17:27:30 +0530

    basic practice to get start

---
+++ First.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class First{
+    public static void main(String args[]){
+        System.out.print("Enter a num to cal Factorial: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        double fact = 1;
+        for(int i = 1; i<= num; i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+        sc.close();
+    }
+}
